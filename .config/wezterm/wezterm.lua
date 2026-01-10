549b45bdd0d961966d82188931eaecf9eee79b54
kinoco313 <koutah130101@gmail.com>
2026-01-10 21:46:31 +0900

    feat: initial rows and cols settings

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,14 +1,15 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local config = wezterm.config_builder()
-
 
 config.automatically_reload_config = true
 config.font_size = 14.0
-config.font = wezterm.font 'Hack Nerd Font'
+config.font = wezterm.font("Hack Nerd Font")
 config.use_ime = true
 config.window_background_opacity = 0.80
 config.window_decorations = "RESIZE"
 config.hide_tab_bar_if_only_one_tab = true
+config.initial_cols = 140
+config.initial_rows = 40
 
 config.window_frame = {
 	inactive_titlebar_bg = "none",
@@ -50,4 +51,3 @@ wezterm.on("format-tab-title", function(tab, tabs, panes, config, hover, max_wid
 end)
 
 return config
-
